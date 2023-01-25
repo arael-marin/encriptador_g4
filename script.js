5e3ff26d0987e7af055f4c11cff3363b56a5c51f
Ing. Jairo Arael Salazar Marín <deneb028@gmail.com>
2023-01-24 20:57:55 -0600

    Iniciando spring de encriptador

--- script.js
+++ script.js
@@ -1,9 +1,70 @@
-document.getElementById("formulario").addEventListener("submit", function (event){
-    event.preventDefault();
+// primer codigo de javasript de encriptador 
 
-var mensaje=document.getElementById("mensaje").value;
+// document.getElementById("formulario").addEventListener("submit", function (event){
+//     event.preventDefault();
 
-var mensajeEncriptado=btoa(mensaje);
+// var mensaje=document.getElementById("mensaje").value;
 
-alert (mensajeEncriptado);
-});
+// var mensajeEncriptado=btoa(mensaje);
+
+// alert (mensajeEncriptado);
+// });
+
+function encriptar(){
+    var textoEntrada = document.getElementById("texto-entrada").value;
+    var textoSalida = "";
+    for (var i = 0; i< textoEntrada.length; i++){
+        switch (textoEntrada[i]){
+            case "e":
+                textoSalida += "enter";
+                break;
+            case "i":
+                textoSalida += "imes";
+                break;
+            case "a":
+                textoSalida += "ai";
+                break;
+            case "o":
+                textoSalida += "ober";
+                break;
+            case "u":
+                textoSalida += "ofat";
+                break;
+            default:
+                textoSalida += textoEntrada[i];
+        }
+    }
+    document.getElementById(texto-salida).value = textoSalida;
+}
+
+function desencriptador(){
+    var textoEntrada= document.getElementById(texto-entrada).value;
+    var textoSalida="";
+    var palabraActual="";
+    for (var i =0; i< textoEntrada.length; i++){
+        palabraActual += textoEntrada[i];
+            switch (palabraActual){
+                case "enter":
+                    textoSalida += "e";
+                    palabraActual = "";
+                    break;
+                case "imes":
+                    textoSalida += "i";
+                    palabraActual = "";
+                    break;
+                case "ai":
+                    textoSalida += "a";
+                    palabraActual = "";
+                    break;
+                case "ober":
+                    textoSalida += "o";
+                    palabraActual = "";
+                    break;
+                case "ufat":
+                    textoSalida += "u";
+                    palabraActual = "";
+                    break;
+            }
+    }
+    document.getElementById("texto-salida").value=textoSalida;
+}
