cc26bd63355cdc705f5bf9c95829911381ec9b14
Ing. Jairo Arael Salazar Marín <deneb028@gmail.com>
2023-01-28 22:17:35 -0600

    creando funcion de encriptacion

--- script.js
+++ script.js
@@ -1,70 +1,28 @@
-// primer codigo de javasript de encriptador 
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat
+// Por ejemplo:
+// "gato" => "gaitober"
+// gaitober" => "gato"
 
-// document.getElementById("formulario").addEventListener("submit", function (event){
-//     event.preventDefault();
+const texto=document.querySelector(".seccion1texttarea");
+const mensaje=document.querySelector(".seccion2mensaje");
+const imagen=document.querySelector(".seccion2img");
+const info=document.querySelector(".seccion2info");
+const copiarbtn=document.querySelector(".botoncopiar");
 
-// var mensaje=document.getElementById("mensaje").value;
-
-// var mensajeEncriptado=btoa(mensaje);
-
-// alert (mensajeEncriptado);
-// });
-
-function encriptar(){
-    var textoEntrada = document.getElementById("texto-entrada").value;
-    var textoSalida = "";
-    for (var i = 0; i< textoEntrada.length; i++){
-        switch (textoEntrada[i]){
-            case "e":
-                textoSalida += "enter";
-                break;
-            case "i":
-                textoSalida += "imes";
-                break;
-            case "a":
-                textoSalida += "ai";
-                break;
-            case "o":
-                textoSalida += "ober";
-                break;
-            case "u":
-                textoSalida += "ufat";
-                break;
-            default:
-                textoSalida += textoEntrada[i];
-        }
-    }
-    document.getElementById("texto-salida").value = textoSalida;
-}
-
-function desencriptar() {
-  var textoEntrada = document.getElementById("texto-entrada").value;
-  var textoSalida = "";
-  for (var i = 0; i < textoEntrada.length; i++) {
-    switch (textoEntrada.substring(i, i + 20)) {
-      case "enter":
-        textoSalida += "e";
-        i += 4;
-        break;
-      case "imes":
-        textoSalida += "i";
-        i += 3;
-        break;
-      case "ai":
-        textoSalida += "a";
-        i += 1;
-        break;
-      case "ober":
-        textoSalida += "o";
-        i += 3;
-        break;
-      case "ufat":
-        textoSalida += "u";
-        i += 3;
-        break;
-      default:
-        textoSalida += textoEntrada[i];
+texto.value="";
+mensaje.value="";
+// funcion para encriptar texto
+function encriptar (textoEncriptado){
+  let llavesEncriptacion =[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+  textoEncriptado = textoEncriptado.toLowerCase();
+  for (let i = 0; i < llavesEncriptacion.length; i++){
+    if (textoEncriptado.includes(llavesEncriptacion[i][0])){
+      textoEncriptado=textoEncriptado.replace.All(llavesEncriptacion[i][0],llavesEncriptacion[i][1]);
     }
   }
-  document.getElementById("texto-salida").value = textoSalida;
+  return textoEncriptado;
 }
